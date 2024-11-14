ec49f56c87c28e106bb5a6ce8275729c305ce311
dbrkic76 <brkicdragan@hotmail.com>
2024-11-14 18:07:58 +0100

    2024-11-44-bom

--- BOM/RickAndMorty/script.js
+++ BOM/RickAndMorty/script.js
@@ -11,18 +11,22 @@ getCharacters();
 function showCharacters(data) {
   console.log(data?.results);
   const characterArray = data.results;
-
   characterArray.forEach((e) => {
     // CREATING ELEMENTS
     const characterCard = document.createElement("div");
     const characterName = document.createElement("h3");
+    const characterLike = document.createElement("button");
     const characterPicture = document.createElement("img");
     // DODAVANJE SADRZAJA U ELEMNTE
     characterName.textContent = e.name;
     characterPicture.setAttribute("src", e.image);
+    characterCard.id = "singleCard";
+    characterLike.id = "likeButton";
+    characterLike.textContent = "LIKE!";
+    characterLike.onclick = 
     // APPEND
     characterCardContainer.append(characterCard);
-    characterCard.append(characterPicture, characterName);
+    characterCard.append(characterPicture, characterName, characterLike);
   });
 }
 showCharacters();
